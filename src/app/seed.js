0089b1d3056fc503fac02b431f21e3a028caaec6
aidyn altay <bover199131@gmail.com>
2019-06-20 02:45:25 +0600

    fstack2App

---
+++ src/app/seed.js
@@ -0,0 +1,54 @@
+export const seedData = [
+  {
+    id: 1,
+    abbvTitle: "Mon",
+    fullTitle: "Monday",
+    events: [
+      { details: "Start new life", edit: false },
+      { details: "Celebrate new life", edit: false }
+    ],
+    active: true
+  },
+  {
+    id: 2,
+    abbvTitle: "Tue",
+    fullTitle: "Tuesday",
+    events: [{ details: "Club night on tuesday:)", edit: false }],
+    active: false
+  },
+  {
+    id: 3,
+    abbvTitle: "Wed",
+    fullTitle: "Wednesday",
+    events: [{ details: "Do something useful", edit: false }],
+    active: false
+  },
+  {
+    id: 4,
+    abbvTitle: "Thu",
+    fullTitle: "Thursday",
+    events: [],
+    active: false
+  },
+  {
+    id: 5,
+    abbvTitle: "Fri",
+    fullTitle: "Friday",
+    events: [{ details: "Friday Party(beer beer beer)", edit: false }],
+    active: false
+  },
+  {
+    id: 6,
+    abbvTitle: "Sat",
+    fullTitle: "Saturday",
+    events: [{ details: "Read the book about sober life", edit: false }],
+    active: false
+  },
+  {
+    id: 7,
+    abbvTitle: "Sun",
+    fullTitle: "Sunday",
+    events: [{ details: "Enjoy life with PC", edit: false }],
+    active: false
+  }
+];
